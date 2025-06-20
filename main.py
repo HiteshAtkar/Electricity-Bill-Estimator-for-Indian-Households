c29010834034e270a8deb853c6ccd19dc055afab
Hitesh <hiteshatkar275@gmail.com>
2025-06-20 20:36:15 +0530

    Added Git workflow

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score,mean_squared_error,mean_absolute_error
+import mlflow.sklearn
+from mlflow.models.signature import infer_signature
+
 
 data=pd.read_csv("data/processed_dataset.csv")
 x=data.iloc[:,:-1]
@@ -15,6 +18,17 @@ mae=mean_absolute_error(y_test,y_pred)
 mse=mean_squared_error(y_test,y_pred)
 r2=r2_score(y_test,y_pred)
 rmse=mse**0.5
+mlflow.set_experiment("LR Test 1")
+with mlflow.start_run(run_name="Test2"):
+    mlflow.log_metrics({
+        "Mean absolute error":mae,
+        "Mean Squared error":mse,
+        "r2":r2
+    })
+    input_example=x_test.iloc[:2]
+    signature=infer_signature(x_test,y_pred)
+    mlflow.sklearn.log_model(sk_model=LR,name="Linear Regression model",input_example=input_example,signature=signature)
+
 print("Mean absolute error =",mae)
 print("Mean Squared error =",mse)
 print("RMSE =",rmse)
